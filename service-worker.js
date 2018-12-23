d666e1e1b16c4f9f0e3ae65baf4b9fb29494067f
ideias-em-crescimento <ideias-em-crescimento@users.noreply.github.com>
2018-12-23 19:44:53 +0000

    Update

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return true;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
